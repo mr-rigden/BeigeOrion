9ce29e64f1adf243f8f6d2c97d79043f68276a8c
Jason <jasonrigden@gmail.com>
2018-02-12 14:44:00 -0800

    Added config

---
+++ config.py
@@ -0,0 +1,20 @@
+CONFIG = {
+    "TITLE": "",
+    "DESCRIPTION": "",
+    "EXTERNAL_LINK_TEXT": "",
+    "EXTERNAL_LINK_URL": "",
+    "BASE_URL": "",
+}
+
+CONFIG["GROUP"] = {
+    "NAME": "",
+    "DESCRIPTION": "",
+}
+
+CONFIG["SUBJECTS"] = [
+    {
+        "NAME": "",
+        "DESCRIPTION": "",
+        "SCREEN_NAME": ""
+    },
+]

6a128b9210ecf7ab06e33ba467f99c7d3ad29636
Jason <jasonrigden@gmail.com>
2018-02-16 10:55:22 -0800

    added new output path

--- render.py
+++ render.py
@@ -4,23 +4,24 @@ import os.path
 from jinja2 import Environment, FileSystemLoader
 
 import quality_control
+from utils import *
 
 file_loader = FileSystemLoader('templates')
 env = Environment(loader=file_loader)
-output_dir = os.path.join(os.getcwd(), 'output')
 
 
 def save_index_page():
+    file_path = os.path.join(CONFIG['OUTPUT_DIR'], "index.html")
     template = env.get_template('index.html')
     output = template.render(CONFIG=CONFIG)
-    with open('output/index.html', 'w') as f:
+    with open(file_path, 'w') as f:
         f.write(output)
 
 
 def save_subject_page(subject):
     data = quality_control.get_full_quality_report(subject['SCREEN_NAME'])
     file_name = subject['SCREEN_NAME'] + ".html"
-    file_path = os.path.join(output_dir, file_name)
+    file_path = os.path.join(CONFIG['OUTPUT_DIR'], file_name)
     template = env.get_template('subject.html')
     output = template.render(CONFIG=CONFIG, subject=subject, data=data)
     with open(file_path, 'w') as f:

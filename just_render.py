fc82df59c1b9f696e4cb4ae9b8bc14d9cbf3caa1
Jason Rigden <jasonrigden@gmail.com>
2018-02-27 09:23:52 -0800

    just render the pages

---
+++ just_render.py
@@ -0,0 +1,10 @@
+import digester
+import quality_control
+import render
+from utils import *
+
+if __name__ == "__main__":
+    logger.info('BeigeOrion is just rendering')
+
+    render.save_index_page()
+    render.save_subject_all_pages()
